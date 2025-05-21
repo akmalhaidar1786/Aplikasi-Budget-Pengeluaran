c56a6ee8689b6643b11e049c8e41c41de7874a45
Reyyhan776 <Raihansyaabid@gmail.com>
2025-05-21 11:11:30 +0700

    Create hapus.go

---
+++ hapus.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func hapusPengeluaran() {
+	tampilkanSemua()
+
+	var index int
+	fmt.Print("Masukkan nomor pengeluaran yang ingin dihapus: ")
+	fmt.Scanln(&index)
+	index--
+
+	if index < 0 || index >= jumlahData {
+		fmt.Println("Nomor tidak valid.")
+		return
+	}
+
+	for i := index; i < jumlahData-1; i++ {
+		pengeluarans[i] = pengeluarans[i+1]
+	}
+
+	jumlahData--
+	fmt.Println("Pengeluaran berhasil dihapus.")
+}
