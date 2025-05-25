fca4a88bb6751dcdd8f86fd1d30812ddfc2d1d5e
akmalhaidar1786 <akmalhaidarutomo@student.telkomuniversity.ac.id>
2025-05-25 23:55:27 +0700

    Update tambah.go

--- tambah.go
+++ tambah.go
@@ -1,20 +1,32 @@
-package main
-
-import "fmt"
-
-func tambahPengeluaran() {
-	var kategori string
-	var jumlah float64
-
-	fmt.Print("Masukkan kategori: ")
-	fmt.Scanln(&kategori)
-	fmt.Print("Masukkan jumlah pengeluaran (Rp): ")
-	fmt.Scanln(&jumlah)
-
+func tambah(app *Aplikasi) {
 	var p Pengeluaran
-	p.Kategori = kategori
-	p.Jumlah = jumlah
-	pengeluarans[jumlahData] = p
-	jumlahData++
-	fmt.Println("Pengeluaran berhasil ditambahkan.")
+	
+	if app.jumlahData >= 250 {
+		fmt.Println("Maaf, sudah mencapai batas maksimal pengeluaran")
+		return
+	}
+
+
+	var ulangi bool = true
+	for ulangi {	
+		fmt.Print("Kategori (Transportasi/Akomodasi/Makanan/Hiburan): ")
+		fmt.Scan(&p.kategori)
+
+		var valid bool 
+		valid = p.kategori == "transportasi" || p.kategori == "akomodasi" || 
+		p.kategori == "makanan" || p.kategori == "Makanan" || p.kategori == "hiburan" || p.kategori == "Hiburan"
+			
+		if valid {
+			ulangi = false
+		} else {
+			fmt.Println("Mohon maaf kategori tidak tersedia.")
+		}
+	}
+
+	fmt.Print("Masukkan jumlah: ")
+	fmt.Scan(&p.jumlah)
+
+	app.data[app.jumlahData] = p
+	app.jumlahData++
+	fmt.Println("Pengeluaran berhasil ditambahkan!")
 }
