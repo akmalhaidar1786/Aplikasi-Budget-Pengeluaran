8bd595a33ab396626eb4d6609221a08c6c70f6c2
akmalhaidar1786 <akmalhaidarutomo@student.telkomuniversity.ac.id>
2025-05-25 23:57:08 +0700

    Update hapus.go

--- hapus.go
+++ hapus.go
@@ -1,19 +1,28 @@
-package main
+func hapus(app *Aplikasi) {
+	var idx int
+	var valid bool
 
-import "fmt"
-
-func hapusPengeluaran() {
-	tampilkanSemua()
-
-	var index int
-	fmt.Print("Masukkan nomor pengeluaran yang ingin dihapus: ")
-	fmt.Scanln(&index)
-	index--
-
-	for i := index; i < jumlahData-1; i++ {
-		pengeluarans[i] = pengeluarans[i+1]
+	if app.jumlahData == 0 {
+		fmt.Println("Belum ada pengeluaran!")
+		return
 	}
 
-	jumlahData--
-	fmt.Println("Pengeluaran berhasil dihapus.")
+	daftar(app)
+	for valid = false; !valid; {
+		fmt.Print("Pilioh nomor 1-", app.jumlahData, ": ")
+		fmt.Scan(&idx)
+		if idx >= 1 && idx <= app.jumlahData {
+			valid = true
+		} else {
+			fmt.Println("Input tidak valid!")
+		}
+	}
+	idx--
+
+	var i int
+	for i = idx; i < app.jumlahData - 1; i++ {
+		app.data[i] = app.data[i + 1]
+	}
+	app.jumlahData--
+	fmt.Println("Pengeluaran berhasil dihapus!")
 }
