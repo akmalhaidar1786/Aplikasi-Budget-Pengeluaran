e8436d25afb4b692a6794d0a3973f2b625b04557
akmalhaidar1786 <akmalhaidarutomo@student.telkomuniversity.ac.id>
2025-05-25 23:57:49 +0700

    Update menu.go

--- menu.go
+++ menu.go
@@ -1,27 +1,11 @@
 func menu() {
-	for {
-		fmt.Println("\n--- MENU ---")
-		fmt.Println("1. Tambah Pengeluaran")
-		fmt.Println("2. Ubah Pengeluaran")
-		fmt.Println("3. Hapus Pengeluaran")
-		fmt.Println("4. Tampilkan Semua")
-		fmt.Println("5. Keluar")
-		fmt.Print("Pilih menu: ")
-
-		var pilihan int
-		fmt.Scanln(&pilihan)
-
-		if pilihan == 1 {
-			tambahPengeluaran()
-		} else if pilihan == 2 {
-			ubahPengeluaran()
-		} else if pilihan == 3 {
-			hapusPengeluaran()
-		} else if pilihan == 4 {
-			tampilkanSemua()
-		} else if pilihan == 5 {
-			fmt.Println("Terima kasih telah menggunakan aplikasi.")
-			return
-		}
-	}
+	fmt.Println("\nMenu Utama:")
+	fmt.Println("1. Tambah Pengeluaran")
+	fmt.Println("2. Edit Pengeluaran")
+	fmt.Println("3. Hapus Pengeluaran")
+	fmt.Println("4. Tampilkan Laporan")
+	fmt.Println("5. Cari Pengeluaran")
+	fmt.Println("6. Urutkan Pengeluaran")
+	fmt.Println("7. Keluar")
+	fmt.Print("Pilih menu: ")
 }
