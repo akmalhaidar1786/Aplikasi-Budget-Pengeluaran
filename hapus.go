51fcb36817c21a49fd3e2fdc1947f76459609840
Reyyhan776 <Raihansyaabid@gmail.com>
2025-06-04 09:15:14 +0700

    Update hapus.go

--- hapus.go
+++ hapus.go
@@ -1,28 +1,28 @@
-func hapus(app *Aplikasi) {
-	var idx int
-	var valid bool
+func hapusPengeluaran(data *[MaksData]Pengeluaran, jumlah *int) {
+	if *jumlah == 0 {
+		fmt.Println("Belum ada data pengeluaran.")
+	} else {
+		fmt.Println("Daftar pengeluaran:")
+		i := 0
+		for i < *jumlah {
+			fmt.Printf("%d. %s - %d\n", i+1, data[i].Kategori, data[i].Jumlah)
+			i = i + 1
+		}
 
-	if app.jumlahData == 0 {
-		fmt.Println("Belum ada pengeluaran!")
-		return
-	}
+		var index int
+		fmt.Print("Masukkan nomor pengeluaran yang ingin dihapus: ")
+		fmt.Scanln(&index)
 
-	daftar(app)
-	for valid = false; !valid; {
-		fmt.Print("Pilioh nomor 1-", app.jumlahData, ": ")
-		fmt.Scan(&idx)
-		if idx >= 1 && idx <= app.jumlahData {
-			valid = true
+		if index >= 1 && index <= *jumlah {
+			i := index - 1
+			for i < *jumlah-1 {
+				data[i] = data[i+1]
+				i = i + 1
+			}
+			*jumlah = *jumlah - 1
+			fmt.Println("Pengeluaran berhasil dihapus.")
 		} else {
-			fmt.Println("Input tidak valid!")
+			fmt.Println("Nomor tidak valid.")
 		}
 	}
-	idx--
-
-	var i int
-	for i = idx; i < app.jumlahData - 1; i++ {
-		app.data[i] = app.data[i + 1]
-	}
-	app.jumlahData--
-	fmt.Println("Pengeluaran berhasil dihapus!")
 }
