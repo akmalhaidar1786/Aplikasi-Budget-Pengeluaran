00b2756d4a3cb2e083386ec886bb739b8f966f61
Reyyhan776 <Raihansyaabid@gmail.com>
2025-06-04 09:21:43 +0700

    Update cariKeluaran.go

--- cariKeluaran.go
+++ cariKeluaran.go
@@ -22,3 +22,40 @@ func cariPengeluaranMenu(data *[MaksData]Pengeluaran, jumlah int) {
 		binarySearch(data, jumlah, kategoriCari)
 	}
 }
+
+func sequentialSearch(data *[MaksData]Pengeluaran, jumlah int, kategoriCari string) {
+	found := false
+	i := 0
+	for i < jumlah {
+		if data[i].Kategori == kategoriCari {
+			fmt.Printf("Data ke-%d: %s - %d\n", i+1, data[i].Kategori, data[i].Jumlah)
+			found = true
+		}
+		i = i + 1
+	}
+	if !found {
+		fmt.Println("Data tidak ditemukan.")
+	}
+}
+
+func binarySearch(data *[MaksData]Pengeluaran, jumlah int, kategoriCari string) {
+	low := 0
+	high := jumlah - 1
+	found := false
+
+	for low <= high {
+		mid := (low + high) / 2
+		if data[mid].Kategori == kategoriCari {
+			fmt.Printf("Data ditemukan di index ke-%d: %s - %d\n", mid+1, data[mid].Kategori, data[mid].Jumlah)
+			found = true
+			low = high + 1
+		} else if data[mid].Kategori < kategoriCari {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	if !found {
+		fmt.Println("Data tidak ditemukan.")
+	}
+}
